1ae1f660213740165150f6069d1df3b31fede025
bruno vasconcellos <bbcvasconcellos@gmail.com>
2022-03-27 22:59:28 -0300

    useFetch async fix

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import axios, { AxiosRequestConfig } from "axios";
 
+// replace api:port with your baseURL
+const api = axios.create({
+  baseURL: 'api:port',
+})
+
 
 export const useFetch = (url: string, options?: AxiosRequestConfig) => {
   const [data, setData] = useState<unknown | null>(null);
@@ -8,16 +13,19 @@ export const useFetch = (url: string, options?: AxiosRequestConfig) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(url, options)
-    .then(res => setData(res.data))
-    .catch(err => {
-      setError(err);
-      throw new Error('Data fetching failed...');
-    })
-    .finally(() => {
-      setIsLoading(false)
-      }
-    )
+    const fetchData = async() => {
+      await api.get(url, options)
+      .then(res => setData(res.data))
+      .catch(err => {
+        setError(err);
+        console.log('Data fetching failed...');
+      })
+      .finally(() => {
+        setIsLoading(false)
+        }
+      )
+    } 
+    fetchData();  
   }, [url, options])
 
   return { data, error, isLoading }
