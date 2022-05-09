d0ae3395870490ed8eefc605a50cc156e7badfb4
bruno vasconcellos <bbcvasconcellos@gmail.com>
2022-05-08 21:04:11 -0300

    added: new hook useFile and updated documentation

--- src/hooks/useFile.tsx
+++ src/hooks/useFile.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from "react";
 
 interface FileNameProps {
   name: string;
@@ -7,20 +7,47 @@ interface FileNameProps {
 interface FileProps {
   file: FileNameProps;
   setFile: Dispatch<SetStateAction<FileNameProps>>;
+  invalidFormat: boolean;
 }
 
 interface UseFileProps {
   children: ReactNode;
+  validator: RegExp | string;
 }
 
 export const FileContext = createContext({} as FileProps)
 
-export const UseFile = ({ children }: UseFileProps) => {
-  const [file, setFile] = useState<FileNameProps>({name: ''})
+const FileProvider = ({ children, validator }: UseFileProps) => {
+  const [file, setFile] = useState<FileNameProps>({ name: '' });
+  const [invalidFormat, setInvalidFormat] = useState(true);
+
+  let validateFile: RegExp | string = '';
+
+  if(typeof validator === 'string') {
+    validateFile = /validator/;
+  } else {
+    validateFile = validator
+  }
+
+  useEffect(() => {
+    if(!validateFile || !file || !file?.name?.match(validateFile)) {
+      setInvalidFormat(true)
+    } else {
+      setInvalidFormat(false)
+    }
+  }, [file, validateFile]);
 
   return (
-    <FileContext.Provider value={{ file, setFile }}>
+    <FileContext.Provider value={{ file, setFile, invalidFormat }}>
       { children }
     </FileContext.Provider>
   )
 }
+
+const useFile = () => {
+  const context = useContext(FileContext);
+
+  return context;
+}
+
+export { useFile, FileProvider }
